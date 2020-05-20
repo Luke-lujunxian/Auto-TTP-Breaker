d5cbd5da3a1f43df7db65624d7afbf8883845543
Junxian_Luke_Lu <32172402+Luke-lujunxian@users.noreply.github.com>
2020-05-20 23:21:08 +0800

    remove useless lines

--- HelperClasses.py
+++ HelperClasses.py
@@ -3,9 +3,12 @@ from nltk.lm import WittenBellInterpolated
 from nltk.lm.util import log_base2
 
 POSSIBLE_BRANCH_THRESHOLD = -10
-CHARS = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'}
+CHARS = {'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f', 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k',
+         'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't', 'U', 'u', 'V', 'v',
+         'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'}
 SYMBOLS = {'!', '"', '\'', '(', ')', '-', '.', '/', '0', ':', ';', '?'}
-DIGITS = {'1','2','3','4','5','6','7','8','9',}
+DIGITS = {'1', '2', '3', '4', '5', '6', '7', '8', '9', }
+
 
 class Layer:
     nextLayer = None
@@ -36,34 +39,22 @@ class Node:
         self.possibleEdges = sortedlist(key=lambda edge: edge.prob)
         self.inEdge = []
 
-
     def generateNodes(self, nextLayer: Layer, cypherChar, model: WittenBellInterpolated):
 
         for i in CHARS:
             if isPrintableChar(ord(i), cypherChar):
                 self.addNodeandEdge(i, chr(ord(i) ^ cypherChar), nextLayer, model)
-            #if isPrintableChar(i + 32, cypherChar):  # Small letters
-            #    self.addNodeandEdge(chr(i + 32), chr((i + 32) ^ cypherChar), nextLayer, model)
-
         for i in DIGITS:
             if isPrintableChar(ord(i), cypherChar):
                 self.addNodeandEdge(i, chr(ord(i) ^ cypherChar), nextLayer, model)
-        #for i in SYMBOLS:
-        #   if isPrintableChar(ord(i), cypherChar):
-        #       self.addNodeandEdge(i, chr(ord(i) ^ cypherChar), nextLayer, model)
-        #for i in range(44, 60):
-        #    if isPrintableChar(i, cypherChar):
-        #        self.addNodeandEdge(chr(i), chr(i ^ cypherChar), nextLayer, model)
         if isPrintableChar(ord(' '), cypherChar):
             self.addNodeandEdge(' ', chr(ord(' ') ^ cypherChar), nextLayer, model)
 
-        #self.possibleEdges.sort(key=lambda edge: edge.prob)
-        #if self.p.__len__() == 0:
         visited = set()
         length = self.possibleEdges.__len__()
         pos = 0
         while pos < length:
-            n:Edge = self.possibleEdges[pos]
+            n: Edge = self.possibleEdges[pos]
             if n.a in visited:
                 self.possibleEdges.remove(n)
                 length -= 1
@@ -71,20 +62,15 @@ class Node:
                 visited.add(n.a)
                 visited.add(n.b)
                 pos += 1
-        index = 0
-            #for e in self.possibleEdges:
-            #    if e.prob >= -20:
-            #        index = self.possibleEdges.index(e)
-            #self.possibleEdges = self.possibleEdges[index:self.possibleEdges.__len__()]
+        self.possibleEdges = self.possibleEdges[int(
+            self.possibleEdges.__len__() * 0.7 if self.possibleEdges.__len__() > 10 else 0):self.possibleEdges.__len__()]
+        # for e in self.possibleEdges:
+        # print(self.p + e.a + "," + self.q + e.b + ": " + str(e.prob))
 
-        #else:
-        self.possibleEdges = self.possibleEdges[int(self.possibleEdges.__len__() *0.7 if self.possibleEdges.__len__() >10 else 0):self.possibleEdges.__len__()]
-        #for e in self.possibleEdges:
-            #print(self.p + e.a + "," + self.q + e.b + ": " + str(e.prob))
     def addNodeandEdge(self, a, b, nextLayer, model):
         score = 0
 
-        if self.p.__len__()>0 and (self.p[-1] == ' ' or self.q[-1] == ' '):
+        if self.p.__len__() > 0 and (self.p[-1] == ' ' or self.q[-1] == ' '):
             ascore = 0
             bscore = 0
             if self.p[-1] == ' ':
@@ -95,43 +81,12 @@ class Node:
                 bscore = model.score(b, context=tokenlize(' '))
             else:
                 bscore = model.score(b, context=tokenlize(self.q))
-            score = log_base2(ascore*bscore)
-
-        #elif self.p.__len__()>0 and ((a == ' ' and self.p[-1] in SYMBOLS) or ( b == ' ' and self.q[-1] in SYMBOLS)):
-        #    ascore = 0
-        #    bscore = 0
-        #    if a == ' ' and self.p[-1] in SYMBOLS:
-        #        ascore = 1
-        #    else:
-        #        ascore = model.score(a, context=tokenlize(self.p))
-        #    if b == ' ' and self.q[-1] in SYMBOLS:
-        #        bscore = 1
-        #    else:
-        #        bscore = model.score(b, context=tokenlize(self.q))
-        #    score = log_base2(ascore*bscore)
-#
-       #elif self.p.__len__()>0 and (b in SYMBOLS) or (a in SYMBOLS):
-       #    ascore = 0
-       #    bscore = 0
-       #    if a in SYMBOLS:
-       #        ascore= model.score(' ', context=tokenlize(self.p))*0.05
-       #    else:
-       #        ascore = model.score(a, context=tokenlize(self.p))
-
-       #    if b in SYMBOLS:
-       #        bscore= model.score(' ', context=tokenlize(self.q))*0.05
-       #    else:
-       #        bscore = model.score(b, context=tokenlize(self.q))
-
-       #    #if ascore == bscore == 1:
-       #    #    score = -100 #POSSIBLE_BRANCH_THRESHOLD+0.001 if self.p.__len__()>=3 else -19.999
-       #    #else:
-       #    score = log_base2(ascore*bscore)
+            score = log_base2(ascore * bscore)
         else:
             score = log_base2(model.score(a, context=tokenlize(self.p)) * model.score(b, context=tokenlize(self.q)))
-        #if self.p.__len__() >= 18:
-        #    print(self.p + a + "," + self.q + b + ": " + str(score))
-        if score > POSSIBLE_BRANCH_THRESHOLD or (self.p.__len__()<3 and score > -20):  # or (self.p == '       ' and self.q == '       ' and score>=-20):
+
+        # print(self.p + a + "," + self.q + b + ": " + str(score))
+        if score > POSSIBLE_BRANCH_THRESHOLD or (self.p.__len__() < 3 and score > -20):
             print(self.p + a + "," + self.q + b + ": " + str(score))
             newNode = Node(self.p + a, self.q + b)
             newEdge = Edge(a, b, self, newNode, score)
@@ -145,26 +100,7 @@ class Node:
 
 def tokenlize(s: str):
     l = list(s)
-    #s2 = ''
-    #for i in range(len(s)):
-    #    if s[i] in SYMBOLS:
-    #        pass
-    #    else:
-    #        s2 += s[i]
-    #l = list(s2)
-    #for i in range(7-s.__len__()):
-    #    l.append('<s>')
-    #if(s.__len__() == 0):
-    #    l.append(" ")
-    #else:
-    #    l.extend(list(s))
-    #for i in range(l.__len__()):
-    #    if l[i] == ' ':
-    #        if i != 0 and l[i-1] != '<s>':
-    #            l[i] = '</s>'
-    #        else:
-    #            l[i] = '<s>'
-    if(s == ''):
+    if s == '':
         l = [' ']
     return l
 
@@ -194,6 +130,7 @@ class Edge:
                 self.prevNode.possibleEdges.remove(self)
             except ValueError:
                 pass
+
 
 def isPrintableChar(a: int, cypherChar: int):
     res = a ^ cypherChar
--- training.py
+++ training.py
@@ -1,16 +1,18 @@
+import dill as pickle
+from nltk import word_tokenize
 from nltk.lm import WittenBellInterpolated
 from nltk.lm.preprocessing import padded_everygram_pipeline
-from nltk.util import everygrams
-from nltk import word_tokenize, sent_tokenize, ngrams
-import dill as pickle
-from nltk.util import pad_sequence
-n = 7
-INTERVAL_OF_SAVING: int = 2000
+
 from HelperClasses import CHARS
 from HelperClasses import DIGITS
+
+n = 7
+INTERVAL_OF_SAVING: int = 2000
+
+
 def train():
     model = WittenBellInterpolated(n)
-    data = open("wikiData.txt",'r',encoding="utf-8")
+    data = open("wikiData.txt", 'r', encoding="utf-8")
     i = 0
     line = ''
 
@@ -20,10 +22,6 @@ def train():
 
     for c in DIGITS:
         vocab.append(c)
-    #for i in range(39, 42):
-    #    vocab.append(chr(i))
-    #for i in range(44, 60):
-    #    vocab.append(chr(i))
     vocab.append(' ')
     vocab.append('<s>')
     vocab.append('</s>')
@@ -34,46 +32,21 @@ def train():
             if linep == '':
                 break
             line = line + linep
-            #temp = word_tokenize(sent)
-            #temp2 = map(str.lower, word_tokenize(sent))
-            i+=1
-            if i%100 == 0:
+            i += 1
+            if i % 100 == 0:
                 print(i)
             if i % INTERVAL_OF_SAVING == 0:
-                tokens = sent_tokenize(line)
-                #s.update(word_tokenize(line))
+                tokens = word_tokenize(line)
                 for j in range(tokens.__len__()):
-               #    if tokens[j] == '~':
-               #        tokens[j] = ''
-               #        continue
-               #    if ord(tokens[j][0]) == 40:
-               #        k = 1
-               #        tokens[j] = ' '+tokens[j]
-               #        while j+k < tokens.__len__() and tokens[j+k][0] != ')':
-               #            if tokens[j+k] == '~':
-               #                k += 1
-               #                continue
-               #            tokens[j] = tokens[j] + tokens[j+k]
-               #            tokens[j+k] = '~'
-               #            k += 1
-               #        tokens[j+k] = tokens[j+k] + ') '
-               #        tokens[j+k] = '~'
-
-
-               #    if j+1 != tokens.__len__():
-               #        if ord(tokens[j+1][0]) in range(65, 90) or ord(tokens[j+1][0]) in range(97, 122)or ord(tokens[j+1][0]) in range(48, 58):
-               #            tokens[j] = ' '+tokens[j]+' '
-               #        elif ord(tokens[j+1][0]) in range(32, 34) or ord(tokens[j+1][0]) in range(39, 40) or ord(tokens[j+1][0]) in range(44,59) or ord(tokens[j+1][0]) == 63:
-               #            tokens[j] = ' '+tokens[j]+tokens[j+1]+' '
-               #            tokens[j+1] = '~'
-               #    else:
-                    tokens[j] = ' '+tokens[j]+' '
-                train_data = padded_everygram_pipeline(n,tokens )
-                model.fit(train_data[0],vocab)
+                    tokens[j] = ' ' + tokens[j] + ' '
+                train_data = padded_everygram_pipeline(n, tokens)
+                for t in train_data[0]:
+                    print(list(t))
+                model.fit(train_data[0], vocab)
                 print(len(model.vocab))
                 line = ''
                 try:
-                    with open('(char&int)kilgariff_ngram_model_'+str(i)+'.pkl', 'wb') as fout:
+                    with open('(char&int)kilgariff_ngram_model_' + str(i) + '.pkl', 'wb') as fout:
                         pickle.dump(model, fout)
                         fout.close()
                 except IOError:
@@ -86,50 +59,37 @@ def train():
 
 def loadModel(path):
     with open(path, 'rb') as fin:
-        return  pickle.load(fin)
+        return pickle.load(fin)
+
 
 def main():
     train()
     test()
 
+
 def test():
     model = loadModel("kilgariff_ngram_model_16000.pkl")
-    #print(model.counts[['a','d','m','i']]['n'])
     string = 'The term I'
     print(list(model.vocab))
     print(string)
-    print( str(model.score(',',context=string.split())) )
-    #for i in range(65, 91):
-    #    print( chr( i )+': '+str(model.score(chr( i ),context=tokenlize(string))) )
-    #    print( chr( i+32 )+': '+str(model.score(chr( i+32 ),context=tokenlize(string))) )
-#
-    #for i in range(32, 35):
-    #    print( chr( i )+': '+str(model.score(chr( i ),context=tokenlize(string))) )
-    #for i in range(39, 42):
-    #    print( chr( i )+': '+str(model.score(chr( i ),context=tokenlize(string))) )
-    #for i in range(44, 60):
-    #    print( chr( i )+': '+str(model.score(chr( i ),context=tokenlize(string))) )
+    print(str(model.score(',', context=string.split())))
+    for i in range(65, 91):
+        print(chr(i) + ': ' + str(model.score(chr(i), context=tokenlize(string))))
+        print(chr(i + 32) + ': ' + str(model.score(chr(i + 32), context=tokenlize(string))))
+    for i in range(32, 35):
+        print(chr(i) + ': ' + str(model.score(chr(i), context=tokenlize(string))))
+    for i in range(39, 42):
+        print(chr(i) + ': ' + str(model.score(chr(i), context=tokenlize(string))))
+    for i in range(44, 60):
+        print(chr(i) + ': ' + str(model.score(chr(i), context=tokenlize(string))))
 
-    #print( chr( 63 )+': '+str(model.score(chr( 63 ),context=tokenlize(string))) )
-
-       #print( chr( 97+i )+': '+str(model.counts['admi'.split()][chr( 97+i )]) )
+    print(chr(63) + ': ' + str(model.score(chr(63), context=tokenlize(string))))
 
 
 def tokenlize(s: str):
     l = list(s)
-    #for i in range(7-s.__len__()):
-    #    l.append('<s>')
-    #if(s.__len__() == 0):
-    #    l.append(" ")
-    #else:
-    #    l.extend(list(s))
-    #for i in range(l.__len__()):
-    #    if l[i] == ' ':
-    #        if i != 0 and l[i-1] != '<s>':
-    #            l[i] = '</s>'
-    #        else:
-    #            l[i] = '<s>'
     return l
+
 
 if __name__ == '__main__':
     main()
